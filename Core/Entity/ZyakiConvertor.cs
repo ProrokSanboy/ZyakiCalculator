e22ebd86bed4a8aff3971208546a6d8bf7e0aa29
Сеня <rainbow.cake10@gmail.com>
2024-03-25 22:43:42 +0300

    Добавьте файлы проекта.

---
+++ Core/Entity/ZyakiConvertor.cs
@@ -0,0 +1,55 @@
+﻿namespace ZyakiCalculator.Core.Entity
+{
+    class ZyakiConvertor(int value)
+    {
+        private readonly int value = value;
+        private readonly List<string> result = [];
+        private readonly Dictionary<string, int> numeration = new()
+        {
+            { "*Шмяк", 10 },
+            { "*Полушмяк", 5 },
+            { "Зяки-зяки", 2 },
+            { "Зяки", 1 }
+        };
+
+        private string MinNumerationName => numeration.MinBy(kvp => kvp.Value).Key;
+
+        public void Process()
+        {
+            int calcValue = value;
+
+            if (calcValue <= 0)
+                return;
+
+            foreach (KeyValuePair<string, int> entry in numeration)
+            {
+                if (calcValue / entry.Value > 0)
+                {
+                    if (entry.Key.Contains('*'))
+                        if (!result.Contains(PrepareName(entry.Key)))
+                            result.Add(PrepareName(MinNumerationName));
+
+                    while (calcValue / entry.Value > 0)
+                    {
+                        int whole = calcValue / entry.Value;
+                        int remainder = calcValue % entry.Value;
+
+                        calcValue = (whole - 1) * entry.Value + remainder;
+
+                        result.Add(PrepareName(entry.Key));
+
+                        if (calcValue == 1)
+                        {
+                            result.Add(PrepareName(MinNumerationName));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public string Result() => result.Count > 0 ? result.Aggregate((a, b) => a + " " + b) : "_|_";
+
+        private static string PrepareName(string name) => name.Replace("*", String.Empty);
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ZyakiCalculator.Core.Entity;
+
+namespace ZyakiCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCalculateClick(object sender, EventArgs e)
+        {
+            int value = Int32.Parse(NumberEntry.Text);
+            ZyakiConvertor convertor = new(value);
+
+            convertor.Process();
+
+            string result = convertor.Result();
+
+            ResultLabel.Text = result;
+            ResultLabel.IsVisible = true;
+        }
+
+        private void OnNumberEntryChanged(object sender, EventArgs e)
+        {
+            ResultLabel.Text = String.Empty;
+            ResultLabel.IsVisible = false;
+        }
+    }
+
+}
